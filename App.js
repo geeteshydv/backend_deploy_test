d1a5d67c6e1367325d4ece732e0dba05cdd3423d
Geetesh Yadav <geeteshyadav12@gmail.com>
2024-06-26 10:21:21 +0530

    changed the url

--- App.js
+++ App.js
@@ -13,8 +13,13 @@ app.use(bodyParser.json());
 
 connectMongoDB("mongodb://localhost:27017/todo-list-database");
 
-app.use("/todos", userRouter);
-
+// app.use("/todos", userRouter);
+app.get(
+  "backend-deploy-test-8k9w6yjx4-moreofgeet25s-projects.vercel",
+  (req, res) => {
+    res.send("Hello World");
+  }
+);
 app.listen(port, () => {
   console.log(`listening on port : ${port}`);
 });
